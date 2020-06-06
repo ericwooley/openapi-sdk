98e57e4fbe960880225619d297fc04446fac201a
Eric Wooley <ericwooley@gmail.com>
2020-06-06 09:01:29 -0700

    publish current version

--- publish.sh
+++ publish.sh
@@ -3,10 +3,11 @@
 ## this requires jq
 
 echo $(dirname $0)
-NEW_VERSION=$(cat package.json | jq .version);
-echo "using version: $NEW_VERSION"
+CURRENT_VERSION=$(cat package.json | jq .version);
+echo "using version: $CURRENT_VERSION"
 nx build openapi-sdk || exit 0;
 yarn np  --no-publish || exit 0;
+NEW_VERSION=$(cat package.json | jq .version);
 BUILD_PACKAGE_JSON="./dist/libs/openapi-sdk/package.json"
 cat $BUILD_PACKAGE_JSON  | jq ".version = $NEW_VERSION" > "$BUILD_PACKAGE_JSON.tmp"
 rm $BUILD_PACKAGE_JSON
