b9de39f3d9422eb6332568738d1e59a735641172
Eric Wooley <ericwooley@gmail.com>
2020-06-06 09:06:55 -0700

    remove np

--- publish.sh
+++ publish.sh
@@ -4,9 +4,8 @@
 
 echo $(dirname $0)
 CURRENT_VERSION=$(cat package.json | jq .version);
-echo "using version: $CURRENT_VERSION"
 nx build openapi-sdk || exit 0;
-yarn np  --no-publish || exit 0;
+yarn version "$@" || exit 0;
 NEW_VERSION=$(cat package.json | jq .version);
 BUILD_PACKAGE_JSON="./dist/libs/openapi-sdk/package.json"
 cat $BUILD_PACKAGE_JSON  | jq ".version = $NEW_VERSION" > "$BUILD_PACKAGE_JSON.tmp"
