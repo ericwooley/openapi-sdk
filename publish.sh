f89332b5398ce4c3c220acaf7c0087952864cd1c
Eric Wooley <ericwooley@gmail.com>
2020-06-07 09:16:44 -0700

    pass only the version to version, rest to publish

--- publish.sh
+++ publish.sh
@@ -5,12 +5,12 @@
 echo $(dirname $0)
 CURRENT_VERSION=$(cat package.json | jq .version);
 nx build openapi-sdk || exit 0;
-npm version "$@" || exit 0;
+npm version $1 || exit 0;
 NEW_VERSION=$(cat package.json | jq .version);
 DEPENDENCIES=$(cat package.json | jq .dependencies)
 BUILD_PACKAGE_JSON="./dist/libs/openapi-sdk/package.json"
 cat $BUILD_PACKAGE_JSON  | jq ".version = $NEW_VERSION | .dependencies = $DEPENDENCIES" > "$BUILD_PACKAGE_JSON.tmp"
 rm $BUILD_PACKAGE_JSON
 mv "$BUILD_PACKAGE_JSON.tmp" $BUILD_PACKAGE_JSON
-npm publish ./dist/libs/openapi-sdk --access public
+npm publish ./dist/libs/openapi-sdk --access public echo "${@:2}"
 git push --tags
