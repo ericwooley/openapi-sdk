f41706c272028337e6c68c093d803817a05c1685
Eric Wooley <ericwooley@gmail.com>
2020-06-07 09:16:49 -0700

    0.2.2

--- publish.sh
+++ publish.sh
@@ -12,5 +12,5 @@ BUILD_PACKAGE_JSON="./dist/libs/openapi-sdk/package.json"
 cat $BUILD_PACKAGE_JSON  | jq ".version = $NEW_VERSION | .dependencies = $DEPENDENCIES" > "$BUILD_PACKAGE_JSON.tmp"
 rm $BUILD_PACKAGE_JSON
 mv "$BUILD_PACKAGE_JSON.tmp" $BUILD_PACKAGE_JSON
-npm publish ./dist/libs/openapi-sdk --access public echo "${@:2}"
+npm publish ./dist/libs/openapi-sdk --access public "${@:2}"
 git push --tags
